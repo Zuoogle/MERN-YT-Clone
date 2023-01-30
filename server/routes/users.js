ceee20934709cf97bc4417b8f4681c38e901a2e4
Sherefedin M Getahun <smgetahun@live.com>
2023-01-29 23:44:24 -0800

    Start working on users controller and routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,18 @@
 import express from "express";
-import {} from '../controllers/users.js'
+import { } from '../controllers/users.js'
 
 const router = express.Router();
+
+//update user
+
+//delete user
+
+//subscribe a user
+
+//unsubscribe a user
+
+//like a video
+
+//dislike a video
 
 export default router;
